b493e2a529416a7ac7cb70068eca70a05bf09b43
PL1664268 <yamaguchi-teppei-yc@ynu.jp>
2018-05-18 22:07:12 +0900

    add Othello_Folder

---
+++ Othello/src/othello/PlayerArrayList.java
@@ -0,0 +1,12 @@
+package othello;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class PlayerArrayList<E> extends ArrayList<E> implements Serializable {
+	private static final long serialVersionUID = 2L;
+
+	PlayerArrayList() {
+		super();
+	}
+}
